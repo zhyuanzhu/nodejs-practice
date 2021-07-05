b6cde00767ee6f61f8e5ea0dfd275827baa6287e
alphazhangyz <alphazhangyz@gmail.com>
2021-07-05 23:49:12 +0800

    抽取错误状态码,统一处理错误状况

--- src/app/errorHandle.js
+++ src/app/errorHandle.js
@@ -1,6 +1,20 @@
+const errorTypes = require('../constants/error-types');
+
 function errorHandler (error, ctx) {
-  ctx.status = 200;
-  ctx.body = error.message;
+  let status, message;
+  switch (error.message) {
+    case errorTypes.NAME_OR_PASSWORD_IS_REQUIRED:
+      status = 400;
+      message = '用户名或者密码不能为空'
+      break;
+  
+    default:
+      status = 404;
+      message = 'NOT FOUND ~'
+      break;
+  }
+  ctx.status = status;
+  ctx.body = message;
 }
 
 module.exports = errorHandler;
--- src/middleware/userMiddleware.js
+++ src/middleware/userMiddleware.js
@@ -1,13 +1,14 @@
+const errorType = require('../constants/error-types');
+
 const verifyUser = async (ctx, next) => {
 
   // 获取用户名和密码
   const { name, password } = ctx.request.body;
-  console.log(name, password);
 
   // 判断用户名和密码不能为空
   if (!name || !password) {
     // 给统一定义一下错误的code和错误信息枚举
-    const error = new Error('用户名或者密码错误!');
+    const error = new Error(errorType.NAME_OR_PASSWORD_IS_REQUIRED);
     return ctx.app.emit('error', error, ctx);
   }
 
