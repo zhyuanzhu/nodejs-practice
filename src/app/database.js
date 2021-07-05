ad23cc117123c954a13b05d3865bf17566768036
alphazhangyz <alphazhangyz@gmail.com>
2021-07-05 22:16:36 +0800

    mysql2 promise 模式插入数据

--- src/app/database.js
+++ src/app/database.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const config = require('./config');
 
@@ -20,6 +20,6 @@ connections.getConnection((err, conn) => {
   });
 });
 
-module.exports = connections.promise;
+module.exports = connections;
 
 
--- src/service/userService.js
+++ src/service/userService.js
@@ -1,7 +1,13 @@
+const connection = require('../app/database')
+
 class UserService {
   async create (user) {
     // 将数组存储到数据库中
-    return `用户 ${user.name} 创建用户成功了，密码是 ${user.password}`;
+    const { name, password } = user
+    const statement = `INSERT INTO users (name, password) VALUES(?, ?);`;
+    const result = await connection.execute(statement, [name, password])
+
+    return result;
   }
 }
 
